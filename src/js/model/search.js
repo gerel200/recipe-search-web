c1bb9bc6819da6903c514b5c27c2637470b62151
gerel <gerelgerelhen@gmail.com>
2025-09-03 15:12:32 +0900

    replace

--- src/js/model/search.js
+++ src/js/model/search.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-
-
 export default class Search{
     constructor(query){
         this.query = query;
@@ -8,7 +6,7 @@ export default class Search{
 
     async doSearch(){
     try{
-        let result= await axios('https://forkify-api.herokuapp.com/api/v2/recipes?search='+this.query);
+        let result= await axios('https://forkify-api.herokuapp.com/api/search?q='+this.query);
         this.result = result.data.recipes;
         return this.result;
     }catch(error){
